6ea4e2d9a3567e4d29a5faf12bb4a0634237b365
Ademuyiwa <ademuyiwatitilayo59@gmail.com>
2025-03-14 20:19:29 +0100

    Updated sidebar for transitions

--- js folder/navbar.js
+++ js folder/navbar.js
@@ -1,8 +1,20 @@
 function showSidebar() {
   const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.add('show');
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.remove('show');
+}
+
+
+/*
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = 'flex'
 }
 function hideSidebar() {
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = 'none'
-}
+} */
