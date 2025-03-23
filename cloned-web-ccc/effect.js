171248738a38563f1b76f9c0f2b7d1120180f38f
Ademuyiwa <ademuyiwatitilayo59@gmail.com>
2025-03-23 01:00:49 +0100

    Text for home-page and auto-typing text

--- cloned-web-ccc/effect.js
+++ cloned-web-ccc/effect.js
@@ -17,7 +17,18 @@ window.addEventListener('scroll', function() {
   }
 });
 
+const typingText = document.querySelector(".sec-text");
 
-
-
-
+const textLoad = () => {
+  setTimeout(() => {
+    typingText.textContent = "WORSHIP";
+  }, 0);
+  setTimeout(() => {
+    typingText.textContent = "COMMUNITY";
+  }, 4000);
+  setTimeout(() => {
+    typingText.textContent = "SERVICE";
+  }, 8000);
+}
+textLoad();
+setInterval(textLoad, 12000);
