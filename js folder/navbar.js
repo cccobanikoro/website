d139e1c1e557921a03ca358117bc7317564ea5df
Ademuyiwa <ademuyiwatitilayo59@gmail.com>
2025-04-02 14:42:32 +0100

    navbar for headers

--- js folder/navbar.js
+++ js folder/navbar.js
@@ -1,3 +1,15 @@
+/*
+let menuList = document.getElementsByClassName("menuList");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+  if (menuList.style.maxHeight === "0px") {
+    menuList.style.maxHeight = "100%";
+  } else {
+    menuList.style.maxHeight = "0px"
+  }
+}
+*/
 
 /*
 function showSidebar() {
