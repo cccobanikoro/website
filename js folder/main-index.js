2191ef3b294cf526c82223a231e4f51da15e575b
Ademuyiwa <ademuyiwatitilayo59@gmail.com>
2025-04-03 19:19:36 +0100

    About section for index

--- js folder/main-index.js
+++ js folder/main-index.js
@@ -1,22 +1,4 @@
-let img = document.getElementById('img-altar');
-let text = document.getElementById('text');
-
-// PARALLAX FOR INDEX 
-window.addEventListener('scroll', () => {
-  let value = window.scrollY;
-
-  text.style.marginTop = value * 2.5 + 'px';
-  img.style.top = value * 1 + 'px';
-});
-
-window.addEventListener('scroll', function() {
-  /*
-  if (window.scrollY > 0) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  } */
-});
+import "./navbar.js";
 
 /*
 const typingText = document.querySelector(".sec-text");
